9dfdcc91c6beb46024877b64ef871634c814758c
Julio Cesar LOPEZ HERRERA <LOPEZHEJ@essilorluxottica.id>
2025-07-29 17:29:51 -0700

    Prueba

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,8 +1,8 @@
 // src/utils/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jbasdsogljwsirdzfifc.supabase.co'; // Reemplaza con tu URL de Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiYXNkc29nbGp3c2lyZHpmaWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzMDk2MDIsImV4cCI6MjA2Mzg4NTYwMn0.bKlWcvq86sLtg_FsGEXP2hTXYhNkVkhkCadhmpZ1v3w'; // Reemplaza con tu clave pública
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   realtime: {
